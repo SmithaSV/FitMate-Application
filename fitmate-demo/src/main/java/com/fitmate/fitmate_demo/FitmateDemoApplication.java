a12c0765e44dc9ee49e8519cb7a0b06eeb57db44
Smitha SV <smitha2892002@gmail.com>
2026-02-02 22:51:22 +0530

    Initial Spring Boot setup with SQL Server and Redis support

---
+++ fitmate-demo/src/main/java/com/fitmate/fitmate_demo/FitmateDemoApplication.java
@@ -0,0 +1,13 @@
+package com.fitmate.fitmate_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitmateDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitmateDemoApplication.class, args);
+	}
+
+}
