dbd7a4b735dfe240079c8bc483da969b40966a3a
Smitha SV <smitha2892002@gmail.com>
2026-02-04 12:08:40 +0530

    added UserRepository

---
+++ src/main/java/com/fitmate/entity/User.java
@@ -0,0 +1,34 @@
+package com.fitmate.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String role; // USER / ADMIN
+
+    @Column(nullable = false)
+    private boolean enabled = true;
+}
+
