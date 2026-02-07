c4d7b653ef577f2d95f0345eee5b9d62b3b8dc5a
Smitha SV <smitha2892002@gmail.com>
2026-02-07 13:06:22 +0530

    added usercontroller with register API

---
+++ src/main/java/com/fitmate/controller/UserController.java
@@ -0,0 +1,29 @@
+package com.fitmate.controller;
+
+import com.fitmate.dto.UserRequestDto;
+import com.fitmate.dto.UserResponseDto;
+import com.fitmate.service.UserService;
+import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+    @PostMapping("/register")
+    public UserResponseDto register(@RequestBody @Valid UserRequestDto requestDto){
+        return userService.registerUser(requestDto);
+    }
+
+
+
+
+
+}
